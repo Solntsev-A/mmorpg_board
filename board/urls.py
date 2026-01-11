f54adb2d454c7cf714411a2090a50aa8129aab54
Solntsev-A <149709154+Solntsev-A@users.noreply.github.com>
2026-01-11 19:29:03 +0300

    add advertisement detail view

--- board/urls.py
+++ board/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from board.views import advertisement_list
+from board.views import AdvertisementDetailView, AdvertisementListView
 
 app_name = 'board'
 
 urlpatterns = [
-    path('', advertisement_list, name='advertisement_list'),
+    path('', AdvertisementListView.as_view(), name='advertisement_list'),
+    path('advertisements/<int:pk>/', AdvertisementDetailView.as_view(), name='advertisement_detail'),
 ]
--- board/views.py
+++ board/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from board.models import Advertisement
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 def advertisement_list(request):
     advertisements = Advertisement.objects.select_related('category', 'author').order_by('-created_at')
@@ -18,3 +18,8 @@ class AdvertisementListView(ListView):
     model = Advertisement
     template_name = 'board/advertisement_list.html'
     context_object_name = 'advertisements'
+
+class AdvertisementDetailView(DetailView):
+    model = Advertisement
+    template_name = 'board/advertisement_detail.html'
+    context_object_name = 'advertisement'
