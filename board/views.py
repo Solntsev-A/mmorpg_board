1d30d657e90612914459be6fb919e1c8339e079a
Solntsev-A <149709154+Solntsev-A@users.noreply.github.com>
2026-01-11 19:18:36 +0300

    display advertisement list with Bootstrap layout

--- board/views.py
+++ board/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from board.models import Advertisement
-
+from django.views.generic import ListView
 
 def advertisement_list(request):
     advertisements = Advertisement.objects.select_related('category', 'author').order_by('-created_at')
@@ -12,3 +12,9 @@ def advertisement_list(request):
             'advertisements': advertisements,
         }
     )
+
+
+class AdvertisementListView(ListView):
+    model = Advertisement
+    template_name = 'board/advertisement_list.html'
+    context_object_name = 'advertisements'
